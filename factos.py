e95be230839ca3d1ebfc4be1c82ac581158b583b
Shubhtiwari7 <85951504+Shubhtiwari7@users.noreply.github.com>
2023-11-06 15:24:24 +0530

    Add files via upload

--- factos.py
+++ factos.py
@@ -1,4 +1,5 @@
 def factorial(n):
-	return 1 if (n==1 or n==0) else n * factorial(n - 1) 
+	return 1 if (n==1 or n==0)
+        else n * factorial(n - 1) 
 num = 5
 print("Factorial of",num,"is",factorial(num))
